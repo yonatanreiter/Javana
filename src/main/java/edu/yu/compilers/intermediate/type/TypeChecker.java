53a11b458f326c816a144367eb8e8b3803df9086
isaacgutt <75759022+isaacgutt@users.noreply.github.com>
2024-04-02 15:48:12 -0400

    Add files via upload

--- src/main/java/edu/yu/compilers/intermediate/type/TypeChecker.java
+++ src/main/java/edu/yu/compilers/intermediate/type/TypeChecker.java
@@ -1,10 +1,37 @@
 package edu.yu.compilers.intermediate.type;
 
+import antlr4.JavanaParser;
+import edu.yu.compilers.intermediate.symtable.Predefined;
+
 /***
  * This class is responsible for checking the types of expressions and statements.
  * Implement your type system here.
  */
 public class TypeChecker {
+
+    public static Typespec returnType(Object o){
+
+        if (o == null) {
+            return null;
+        }
+
+        // Example: Handling literals directly.
+        if (o instanceof Integer) {
+            return Predefined.integerType;
+        } else if (o instanceof Float) {
+            return Predefined.realType;
+        } else if (o instanceof Boolean) {
+            return Predefined.booleanType;
+        } else if (o instanceof String) {
+            return Predefined.stringType;
+        }
+
+
+
+
+        return null;
+
+    }
 
 
 }
